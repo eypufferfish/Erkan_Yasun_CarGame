dd2783e0196e59a916fa0e4e017a0d117a07fbd0
eypufferfish <ey.pufferfish@gmail.com>
2021-04-14 23:17:15 +0300

    code refactoring

--- Assets/Scripts/Model/GameArea.cs
+++ Assets/Scripts/Model/GameArea.cs
@@ -8,12 +8,10 @@ namespace Mobge.CarGame.ErkanYasun.Model
     {
         [SerializeField]
         [Header("x Ekseni [-8..8], y Ekseni [-4..4]")]
-        private SerializableDictionary<Vector2, Obstacle> obstacles = new SerializableDictionary<Vector2, Obstacle>();
+        private readonly SerializableDictionary<Vector2, Obstacle> obstacles = new SerializableDictionary<Vector2, Obstacle>();
 
         [SerializeField]
         private List<CarPathPair> carPathPairs;
-
-        private int activeCarPathIndex;
 
         public List<CarPathPair> CarPathPairs => carPathPairs;
         public SerializableDictionary<Vector2, Obstacle> Obstacles => obstacles;
